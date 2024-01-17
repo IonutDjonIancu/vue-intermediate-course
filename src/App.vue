cd8bdbfc26d9bb3e423f3fca7ddf406778c9bce6
iancu ionut <iiancu85@gmail.com>
2024-01-17 22:37:17 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <section>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+  </section>
 </template>
 
 <style lang="scss">
