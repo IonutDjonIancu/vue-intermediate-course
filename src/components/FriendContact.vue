8b87345a1ad2c9e6a6237cc03c194705415fa418
iancu ionut <iiancu85@gmail.com>
2024-01-18 20:15:24 +0100

    Added some first components

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <li>
+      <h4>{{ friend.name }}</h4>
+      <button @click="toggleDetails">Show details</button>
+      <ul>
+        <li>Phone: {{ friend.phone }}</li>
+        <li>Email: {{ friend.email }}</li>
+      </ul>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FriendComponent",
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  props: {
+    friend: Object,
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h4>My friends</h4>
+    <ul>
+      <FriendContact
+        :key="friend.id"
+        v-for="friend in friends"
+        v-bind:friend="friend"
+      ></FriendContact>
+    </ul>
+  </div>
+</template>
+
+<script>
+import FriendContact from "./FriendContact.vue";
+export default {
+  name: "HomeComponent",
+  components: {
+    FriendContact,
+  },
+  data() {
+    return {
+      friends: [
+        {
+          id: 1,
+          name: "Mark",
+          phone: "1234 5678 90",
+          email: "manuel@gmail.com",
+        },
+        {
+          id: 2,
+          name: "Jane",
+          phone: "1234 5678 91",
+          email: "jane@gmail.com",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
